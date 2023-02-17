3ab2cec207a09b1474266847df36dc8876230807
Radhika Arora <aradhika880@gmail.com>
2023-02-18 02:29:48 +0530

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello, I'm</h5>
+        <h5>Hello there, I'm</h5>
         <h1>Radhika Arora</h1>
         <h5 className="text-light">Software Engineer</h5>
         <CTA/>
